e04d8a6c48c236f2cdfe12d544bccc479a94ccb7
opatut <opatutlol@aol.com>
2011-05-16 20:32:21 +0200

    Created tests setup

---
+++ tests/events/src/main.cpp
@@ -0,0 +1,11 @@
+#include "Root.hpp"
+
+#include "utils/Logger.hpp"
+
+int main() {
+
+    dt::Logger::Get().Info("Well here we are again");
+    dt::Logger::Get().Debug("It's always such a pleasure");
+
+    return 0;
+}
