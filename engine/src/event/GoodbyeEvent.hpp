86c3c0dddefee3d8ff174d108e2c560cdbc90a33
opatut <opatutlol@aol.com>
2011-06-02 20:25:03 +0200

    Documented GoodbyeEvent

--- engine/src/event/GoodbyeEvent.hpp
+++ engine/src/event/GoodbyeEvent.hpp
@@ -5,14 +5,31 @@
 
 namespace dt {
 
+/**
+  * An Event announcing the end of a Connection. Sent by the NetworkManager on disconnect.
+  */
 class GoodbyeEvent : public NetworkEvent {
 public:
+    /**
+      * Advanced constructor.
+      * @param reason The reason for the disconnect.
+      */
     GoodbyeEvent(const std::string& reason = "");
+
     const std::string GetType() const;
     NetworkEvent* NewInstance() const;
     void Serialize(IOPacket& p);
 
+    /**
+      * Returns the reason for the disconnect.
+      * @returns The reason for the disconnect.
+      */
     const std::string& GetReason() const;
+
+    /**
+      * Sets the reason for the disconnect.
+      * @param reason The reason for the disconnect.
+      */
     void SetReason(const std::string& reason);
 private:
     std::string mReason;
