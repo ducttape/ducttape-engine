adf9fc982bb36dc9b0d4b70e2ab1f913e898c224
opatut <opatutlol@aol.com>
2011-06-02 22:23:09 +0200

    Documented EventManager

--- engine/src/event/EventManager.hpp
+++ engine/src/event/EventManager.hpp
@@ -8,17 +8,38 @@
 
 namespace dt {
 
+/**
+  * Class holding all EventListeners, which is responsible to send all Events through the
+  * Event system.
+  * @see EventListener
+  * @see Event
+  */
 class EventManager {
 public:
+    /**
+      * Default constructor.
+      */
     EventManager();
 
+    /**
+      * Method used to initiate an Event being sent to all EventListeners.
+      * @param event The Event to be sent.
+      */
     void HandleEvent(Event* event);
 
+    /**
+      * Adds an EventListener to the list of listeners.
+      * @param listener The EventListener to add.
+      */
     void AddListener(EventListener* listener);
 
+    /**
+      * Removes an EventListener from the list of listeners.
+      * @param listener The EventListener to remove.
+      */
     void RemoveListener(EventListener* listener);
 private:
-    std::vector<EventListener*> mListeners;
+    std::vector<EventListener*> mListeners; //!< The list of EventListeners.
 };
 
 }
