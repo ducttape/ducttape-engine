4e6fbe03ad262a8b154461692b307190f0e8feb7
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-06-03 14:34:38 +0200

    ResourceManager.hpp: Improve documentation

--- engine/src/resources/ResourceManager.hpp
+++ engine/src/resources/ResourceManager.hpp
@@ -15,6 +15,7 @@ namespace dt {
 
 /**
   * Manager for loading resources to memory and communicating resources to Ogre.
+  * @todo This manager should eventually only allow adding of locations which are managed automatically.
   */
 class ResourceManager {
 public:
@@ -66,8 +67,8 @@ public:
     const sf::SoundBuffer& GetSoundBuffer(const std::string& sound_key);
  
 private:
-	boost::ptr_map<std::string, sf::Music> mMusic;
-	boost::ptr_map<std::string, sf::SoundBuffer> mSoundBuffers;
+	boost::ptr_map<std::string, sf::Music> mMusic; //!< Pool of registered music objects. This does not actually contain the music data since music is actually streamed.
+	boost::ptr_map<std::string, sf::SoundBuffer> mSoundBuffers; //!< Pool of registered sound buffers. These are in fact loaded into memory.
 };
 
 }
