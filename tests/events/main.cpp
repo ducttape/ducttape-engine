b1d5625cf77d0d852a5257c34e512aa518ed3652
opatut <opatutlol@aol.com>
2011-06-03 03:01:03 +0200

    Implemented EventManager and Event test

--- tests/events/main.cpp
+++ tests/events/main.cpp
@@ -2,12 +2,59 @@
 
 #include "utils/Logger.hpp"
 
+#include "event/Event.hpp"
+#include "event/EventListener.hpp"
+
+class TestEvent : public dt::Event {
+public:
+    const std::string GetType() const  {
+        return "testevent";
+    }
+
+    void TestEventReceived() {
+        mHasBeenReceivedCorrectly = true;
+    }
+
+    bool mHasBeenReceivedCorrectly;
+};
+
+class TestEventListener : public dt::EventListener {
+public:
+    void HandleEvent(dt::Event* e) {
+        if(e->GetType() == "testevent") {
+            ((TestEvent*)e)->TestEventReceived();
+        }
+        mHasReceivedAnEvent = true;
+    }
+
+    bool mHasReceivedAnEvent;
+};
+
 int main() {
-    dt::Root::get_mutable_instance().Initialize();
+    dt::Root& root = dt::Root::get_mutable_instance();
+    root.Initialize();
 
-    dt::Logger::Get().Info("Well here we are again");
-    dt::Logger::Get().Debug("It's always such a pleasure");
+    root.GetStringManager()->Add("testevent");
 
-    dt::Root::get_mutable_instance().Deinitialize();
+    TestEventListener listener;
+    TestEvent event;
+
+    root.GetEventManager()->AddListener(&listener);
+    root.GetEventManager()->HandleEvent(&event);
+
+    if(!listener.mHasReceivedAnEvent) {
+        std::cerr << "The EventListener has not received any event." << std::endl;
+        root.Deinitialize();
+        return 0;
+    }
+
+    if(!event.mHasBeenReceivedCorrectly) {
+        std::cerr << "The Event has not been recognized as a TestEvent." << std::endl;
+        root.Deinitialize();
+        return 0;
+    }
+
+    std::cout << "Events: OK" << std::endl;
+    root.Deinitialize();
     return 0;
 }
