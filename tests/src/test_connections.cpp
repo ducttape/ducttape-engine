4fb99d059ff0ce3047911a18919441f5d990ae0c
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-06-03 19:55:35 +0200

    test_connections.cpp: Use proper English and more accurate phrases

--- tests/src/test_connections.cpp
+++ tests/src/test_connections.cpp
@@ -35,36 +35,36 @@ int main() {
            if(!(connections[connection_id]->GetIPAddress() == connections_manager.GetConnection(connection_id)->GetIPAddress() && \
                        connections[connection_id]->GetPort() == connections_manager.GetConnection(connection_id)->GetPort())) {
                broken = true;
-               std::cerr << "ConnectionsManager: connections should be equaly." << std::endl;
+               std::cerr << "ConnectionsManager: connections should be equal" << std::endl;
            } else {
                // Test GetConnectionID()
                dt::Connection* tmp_connection = connections_manager.GetConnection(connection_id);
                if(tmp_connection != nullptr) {
                    if(connection_id != connections_manager.GetConnectionID(*(tmp_connection))) {
                        broken = true;
-                       std::cerr << "ConnectionsManager: connection id should be equal." << std::endl;
+                       std::cerr << "ConnectionsManager: connection ids should be equal" << std::endl;
                    }
                } else {
                    broken = true;
-                   std::cerr << "ConnectionsManager: does not know connection id." << std::endl;
+                   std::cerr << "ConnectionsManager: does not know connection id" << std::endl;
                }
            }
 
-           if(i == 20) {
+           if(i >= max_connections) {
                broken = true;
-               std::cerr << "ConnectionsManager: MaxConnections not working properly." << std::endl;
+               std::cerr << "ConnectionsManager: MaxConnections not working properly" << std::endl;
            }
 
         } else {
-            if(i != 20) {
+            if(i != max_connections) {
                 broken = true;
-                std::cerr << "ConnectionsManager: failed on adding connection." << std::endl;
+                std::cerr << "ConnectionsManager: failed on adding connection" << std::endl;
             }
         }
     }
 
     if(broken) {
-        std::cerr << "ConnectionsManager: FAIL - test canceld" << std::endl;
+        std::cerr << "ConnectionsManager: FAIL - further tests canceled" << std::endl;
         return 2;
     }
 
@@ -72,7 +72,7 @@ int main() {
     for(std::pair<uint16_t, std::shared_ptr<dt::Connection>> c : connections) {
         if(!connections_manager.IsKnownConnection(*(c.second.get()))) {
             broken = true;
-            std::cerr << "ConnectionsManager: should know the requested connection." << std::endl;
+            std::cerr << "ConnectionsManager: should know the requested connection" << std::endl;
         }
     }
 
@@ -83,13 +83,13 @@ int main() {
             connections_manager.RemoveConnection(c.first);
             if(connections_manager.GetConnection(c.first) != nullptr) {
                 broken = true;
-                std::cerr << "ConnectionsManager: no proper deletion. RemoveConnection(c.first) // by ID" << std::endl;
+                std::cerr << "ConnectionsManager: no proper deletion using RemoveConnection(c.first) // by ID" << std::endl;
             }
         } else {
             connections_manager.RemoveConnection(*(c.second.get()));
             if(connections_manager.GetConnectionID(*(c.second.get())) != 0) {
                 broken = true;
-                std::cerr << "ConnectionsManager: no proper deletion. RemoveConnection(*(c.second.get())) // by copy" << std::endl;
+                std::cerr << "ConnectionsManager: no proper deletion using RemoveConnection(*(c.second.get())) // by copy" << std::endl;
             }
         }
         ++i;
