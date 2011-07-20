8900e7a32021f4d385f6e514c6c69544a74af759
tomt610 <tomt610@gmail.com>
2011-07-20 05:16:02 +0200

    feature #29, only code no comments, added plane, sphere, box to meshcomponent

--- engine/src/component/MeshComponent.hpp
+++ engine/src/component/MeshComponent.hpp
@@ -10,6 +10,11 @@
 #define DUCTTAPE_ENGINE_COMPONENT_MESHCOMPONENT
 
 #include <OGRE/OgreEntity.h>
+#include <OgrePlane.h>
+#include <OgreSceneNode.h>
+#include <OgreEntity.h>
+#include <OgreMeshManager.h>
+#include <OgrePrefabFactory.h>
 
 #include "component/Component.hpp"
 
@@ -27,6 +32,41 @@ public:
       * @see Component
       */
     MeshComponent(const std::string& name, const std::string& mesh_file);   // TODO: Figure out way to have default name here
+
+    //************************************
+    // Method:    PlaneMesh
+    // FullName:  dt::MeshComponent::PlaneMesh
+    // Access:    public static 
+    // Returns:   MeshComponent*
+    // Qualifier:
+    // Parameter: Ogre::Real a
+    // Parameter: Ogre::Real b
+    // Parameter: Ogre::Real c
+    // Parameter: Ogre::Real d
+    // Parameter: const std::string & name
+    //************************************
+    static MeshComponent* PlaneMesh(Ogre::Real a = 1.0, Ogre::Real b = 1.0, Ogre::Real c = 1.0, Ogre::Real d = 1.0,
+                                    Ogre::Real width = 1.0, Ogre::Real height = 1.0, int x_segments = 20, 
+                                    int y_segments = 20, bool normals = true, unsigned short num_tex_coord_sets = 1, 
+                                    Ogre::Real u_tile = 1.0, Ogre::Real v_tile = 1.0, const std::string& name = "");
+
+    //************************************
+    // Method:    BoxMesh
+    // FullName:  dt::MeshComponent::BoxMesh
+    // Access:    public static 
+    // Returns:   MeshComponent*
+    // Qualifier:
+    // Parameter: size_t l
+    // Parameter: size_t t
+    // Parameter: size_t ff
+    // Parameter: size_t r
+    // Parameter: size_t b
+    // Parameter: size_t bb
+    // Parameter: std::string & name
+    //************************************
+    static MeshComponent* BoxMesh(Ogre::Real width = 1, Ogre::Real height = 1, Ogre::Real depth = 1, const std::string& name = "");
+
+    static MeshComponent* SphereMesh(Ogre::Real radius = 1.0, const std::string& name = "");
 
     virtual void HandleEvent(std::shared_ptr<Event> e);
 
@@ -92,17 +132,37 @@ private:
       */
     void _LoadMesh();
 
+    void _LoadPlane();
+    void _LoadBox();
+    void _LoadSphere();
+
     /**
       * Private method. Destroys the mesh.
       */
     void _DestroyMesh();
-
     Ogre::SceneNode* mSceneNode;    //!< The scene Node the mesh is being attached to.
     Ogre::Entity* mEntity;          //!< The actual mesh.
+    Ogre::Plane* mPlane;
+    bool mIsBox;
+    bool mIsSphere;
+
     Ogre::AnimationState* mAnimationState;  //!< The current animation state.
     bool mLoopAnimation;            //!< Whether the animation shall be looped.
 
     std::string mMeshFile;          //!< The filename of the mesh file.
+
+    Ogre::Real mPlaneWidth;
+    Ogre::Real mPlaneHeight;
+    int mPlaneXSegments;
+    int mPlaneYSegments;
+    bool mPlaneNormals;
+    unsigned short mPlaneNumTexCoordSets;
+    Ogre::Real mPlaneUTile;
+    Ogre::Real mPlaneVTile;
+
+    Ogre::Vector3 mBoxDimenstions;
+
+    Ogre::Real mSphereRadius;
 };
 
 }
