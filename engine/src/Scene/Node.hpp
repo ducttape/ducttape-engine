844c0e6b07825bdec381b901e508e28b18b0d44b
EternalWind <eternalwind@live.cn>
2011-10-22 15:53:32 +0800

    Add uuid to Node.

--- engine/src/Scene/Node.hpp
+++ engine/src/Scene/Node.hpp
@@ -287,6 +287,7 @@ private:
     Ogre::Quaternion mRotation;     //!< The Node rotation.
     Node* mParent;                  //!< A pointer to the parent Node.
     bool mIsUpdatingAfterChange;    //!< Whether the node is just in the process of updating all components after a change occurred. This is to prevent infinite stack loops.
+    boost::uuids::uuid mId;         //!< The node's uuid.
 
 };
 
