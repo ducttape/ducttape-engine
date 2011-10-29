942698b553df2e9ff279d61ff1733d8bd117cf08
EternalWind <eternalwind@live.cn>
2011-10-30 00:52:10 +0800

    FPSPlayerComponent Done!(Can't fire for now)
    Thx to djdduty and clynamen.

--- engine/src/Logic/FPSPlayerComponent.cpp
+++ engine/src/Logic/FPSPlayerComponent.cpp
@@ -18,7 +18,15 @@ namespace dt {
 
 FPSPlayerComponent::FPSPlayerComponent(const QString& name)
     : mBtController(nullptr),
-      mBtGhostObject(nullptr) {}
+      mBtGhostObject(nullptr),
+      mMouseEnabled(true),
+      mMouseSensitivity(1.0),
+      mMouseYInversed(false),
+      mMove(0.0, 0.0, 0.0),
+      mMoveSpeed(5.0f),
+      mWASDEnabled(true),
+      mArrowsEnabled(true),
+      mJumpEnabled(true) {}
 
 void FPSPlayerComponent::OnCreate() {
     btTransform  start_trans;
@@ -27,23 +35,35 @@ void FPSPlayerComponent::OnCreate() {
     start_trans.setRotation(BtOgre::Convert::toBullet(GetNode()->GetRotation(Node::SCENE)));
 
     btScalar character_height = 1.75;
-    btScalar character_width = 1.75;
+    btScalar character_width = 0.44;
     btConvexShape* capsule = new btCapsuleShape(character_width, character_height);
 
     mBtGhostObject = new btPairCachingGhostObject();
     mBtGhostObject->setWorldTransform(start_trans);
     mBtGhostObject->setCollisionShape(capsule);
     mBtGhostObject->setCollisionFlags(btCollisionObject::CF_CHARACTER_OBJECT);
+    btGhostPairCallback* ghost_pair_callback = new btGhostPairCallback();
     mBtGhostObject->setUserPointer((void *)this);
 
-    mBtController = new btKinematicCharacterController(mBtGhostObject, capsule, 0.35);
-    GetNode()->GetScene()->GetPhysicsWorld()->GetBulletWorld()->addCollisionObject(mBtGhostObject,btBroadphaseProxy::CharacterFilter, btBroadphaseProxy::StaticFilter|btBroadphaseProxy::DefaultFilter);
+    mBtController = new btKinematicCharacterController(mBtGhostObject, capsule, 0);
+    GetNode()->GetScene()->GetPhysicsWorld()->GetBulletWorld()->addCollisionObject(mBtGhostObject,btBroadphaseProxy::CharacterFilter, btBroadphaseProxy::StaticFilter | btBroadphaseProxy::DefaultFilter);
     GetNode()->GetScene()->GetPhysicsWorld()->GetBulletWorld()->addAction(mBtController);
+    GetNode()->GetScene()->GetPhysicsWorld()->GetBulletWorld()->getPairCache()->setInternalGhostPairCallback(ghost_pair_callback);
 
     if(!QObject::connect(InputManager::Get(), SIGNAL(sKeyPressed(const OIS::KeyEvent&)), 
         this, SLOT(_HandleKeyPressed(const OIS::KeyEvent&)))) {
             Logger::Get().Error("Cannot connect the key pressed signal with " + GetName()
                 + "'s keyboard input handling slot.");
+    }
+    if(!QObject::connect(InputManager::Get(), SIGNAL(sKeyReleased(const OIS::KeyEvent&)), 
+        this, SLOT(_HandleKeyReleased(const OIS::KeyEvent&)))) {
+            Logger::Get().Error("Cannot connect the key released signal with " + GetName()
+                + "'s keyboard input handling slot.");
+    }
+    if(!QObject::connect(InputManager::Get(), SIGNAL(sMouseMoved(const OIS::MouseEvent&)), 
+        this, SLOT(_HandleMouseInput(const OIS::MouseEvent&)))) {
+            Logger::Get().Error("Cannot connect the mouse moved signal with " + GetName()
+                + "'s mouse input handling slot.");
     }
 }
 
@@ -54,42 +74,158 @@ void FPSPlayerComponent::OnDestroy() {
         delete mBtGhostObject;
 }
 
-void FPSPlayerComponent::OnEnable() {}
+void FPSPlayerComponent::OnEnable() {
+    SetWASDEnabled(true);
+    SetArrowsEnabled(true);
+    SetJumpEnabled(true);
+}
 
-void FPSPlayerComponent::OnDisable() {}
+void FPSPlayerComponent::OnDisable() {
+    SetWASDEnabled(false);
+    SetArrowsEnabled(false);
+    SetJumpEnabled(false);
+}
 
 void FPSPlayerComponent::OnUpdate(double time_diff) {
-    btTransform trans;
+    static Ogre::Vector3 move;
+    static Ogre::Quaternion quaternion;
+    static btTransform trans;
+
+    quaternion = Ogre::Quaternion(GetNode()->GetRotation().getYaw(), Ogre::Vector3(0.0, 1.0, 0.0));
+    move = quaternion * BtOgre::Convert::toOgre(mMove) * mMoveSpeed;
+    mBtController->setVelocityForTimeInterval(BtOgre::Convert::toBullet(move), 0.5);
+
     trans = mBtGhostObject->getWorldTransform();
 
     GetNode()->SetPosition(BtOgre::Convert::toOgre(trans.getOrigin()), Node::SCENE);
-    GetNode()->SetRotation(BtOgre::Convert::toOgre(trans.getRotation()), Node::SCENE);
+}
+
+void FPSPlayerComponent::SetWASDEnabled(bool wasd_enabled) {
+    mWASDEnabled = wasd_enabled;
+}
+
+bool FPSPlayerComponent::GetWASDEnabled() const {
+    return mWASDEnabled;
+}
+
+void FPSPlayerComponent::SetArrowsEnabled(bool arrows_enabled) {
+    mArrowsEnabled = arrows_enabled;
+}
+
+bool FPSPlayerComponent::GetArrowsEnabled() const {
+    return mArrowsEnabled;
+}
+
+void FPSPlayerComponent::SetMoveSpeed(float move_speed) {
+    mMoveSpeed = move_speed;
+}
+
+float FPSPlayerComponent::GetMoveSpeed() const {
+    return mMoveSpeed;
+}
+
+void FPSPlayerComponent::SetMouseEnabled(bool mouse_enabled) {
+    mMouseEnabled = mouse_enabled;
+}
+
+bool FPSPlayerComponent::GetMouseEnabled() const {
+    return mMouseEnabled;
+}
+
+void FPSPlayerComponent::SetMouseSensitivity(float mouse_sensitivity) {
+    mMouseSensitivity = mouse_sensitivity;
+}
+
+float FPSPlayerComponent::GetMouseSensitivity() const {
+    return mMouseSensitivity;
+}
+
+void FPSPlayerComponent::SetMouseYInversed(bool mouse_y_inversed) {
+    mMouseYInversed = mouse_y_inversed;
+}
+
+bool FPSPlayerComponent::GetMouseYInversed() const {
+    return mMouseYInversed;
+}
+
+void FPSPlayerComponent::SetJumpEnabled(bool jump_enabled) {
+    mJumpEnabled = jump_enabled;
+}
+
+bool FPSPlayerComponent::GetJumpEnabled() const{
+    return mJumpEnabled;
 }
 
 void FPSPlayerComponent::_HandleKeyPressed(const OIS::KeyEvent& event) {
-    /*btTransform trans;
-    trans.setIdentity();
-    btVector3 vec(0.0f, 0.0f, 0.0f);
-
-    if(event.key == OIS::KC_W) {
-        vec.setZ(-1.0f);
-
+    if(mWASDEnabled || mArrowsEnabled) {
+        if((event.key == OIS::KC_W && mWASDEnabled) || (event.key == OIS::KC_UP && mArrowsEnabled)) {
+            mMove.setZ(mMove.getZ() - 1.0f);
+        }
+        if((event.key == OIS::KC_S && mWASDEnabled) || (event.key == OIS::KC_DOWN && mArrowsEnabled)) {
+            mMove.setZ(mMove.getZ() + 1.0f);
+        }
+        if((event.key == OIS::KC_A && mWASDEnabled) || (event.key == OIS::KC_LEFT && mArrowsEnabled)) {
+            mMove.setX(mMove.getX() - 1.0f);
+        }
+        if((event.key == OIS::KC_D && mWASDEnabled) || (event.key == OIS::KC_RIGHT && mArrowsEnabled)) {
+            mMove.setX(mMove.getX() + 1.0f);
+        }
     }
-    if(event.key == OIS::KC_S) {
-        vec.setZ(1.0f);
-    }
-    if(event.key == OIS::KC_A) {
-        vec.setX(-1.0f);
-    }
-    if(event.key == OIS::KC_D) {
-        vec.setX(1.0f);
-    }
-    if(event.key == OIS::KC_SPACE) {
+    if(mJumpEnabled && event.key == OIS::KC_SPACE) {
         mBtController->jump();
     }
+}
 
-    trans.setOrigin(vec);*/
-    //mBtGhostObject->setWorldTransform(trans * mBtGhostObject->getWorldTransform());
+void FPSPlayerComponent::_HandleMouseInput(const OIS::MouseEvent& event) {
+    if(mMouseEnabled) {
+        float factor = mMouseSensitivity * -0.01;
+
+        float dx = event.state.X.rel * factor;
+        float dy = event.state.Y.rel * factor * (mMouseYInversed ? -1 : 1);
+
+        if(dx != 0 || dy != 0) {
+            // watch out for da gimbal lock !!
+
+            Ogre::Matrix3 orientMatrix;
+            GetNode()->GetRotation().ToRotationMatrix(orientMatrix);
+
+            Ogre::Radian yaw, pitch, roll;
+            orientMatrix.ToEulerAnglesYXZ(yaw, pitch, roll);
+
+            pitch += Ogre::Radian(dy);
+            yaw += Ogre::Radian(dx);
+
+            // do not let it look completely vertical, or the yaw will break
+            if(pitch > Ogre::Degree(89.9))
+                pitch = Ogre::Degree(89.9);
+
+            if(pitch < Ogre::Degree(-89.9))
+                pitch = Ogre::Degree(-89.9);
+
+            orientMatrix.FromEulerAnglesYXZ(yaw, pitch, roll);
+
+            Ogre::Quaternion rot;
+            rot.FromRotationMatrix(orientMatrix);
+            GetNode()->SetRotation(rot);
+        }
+    }
+}
+
+void FPSPlayerComponent::_HandleKeyReleased(const OIS::KeyEvent& event) {
+    if(mWASDEnabled || mArrowsEnabled) {
+        if((event.key == OIS::KC_W && mWASDEnabled) || (event.key == OIS::KC_UP && mArrowsEnabled)) {
+            mMove.setZ(mMove.getZ() + 1.0f);
+        }
+        if((event.key == OIS::KC_S && mWASDEnabled) || (event.key == OIS::KC_DOWN && mArrowsEnabled)) {
+            mMove.setZ(mMove.getZ() - 1.0f);
+        }
+        if((event.key == OIS::KC_A && mWASDEnabled) || (event.key == OIS::KC_LEFT && mArrowsEnabled)) {
+            mMove.setX(mMove.getX() + 1.0f);
+        }
+        if((event.key == OIS::KC_D && mWASDEnabled) || (event.key == OIS::KC_RIGHT && mArrowsEnabled)) {
+            mMove.setX(mMove.getX() - 1.0f);
+        }
+    }
 }
 
 }
--- engine/src/Logic/FPSPlayerComponent.hpp
+++ engine/src/Logic/FPSPlayerComponent.hpp
@@ -15,6 +15,7 @@
 
 #include <BulletCollision/CollisionShapes/btConvexHullShape.h>
 #include <btBulletDynamicsCommon.h>
+#include <btBulletCollisionCommon.h>
 #include <BulletCollision/CollisionShapes/btHeightfieldTerrainShape.h>
 #include <BulletCollision/CollisionDispatch/btGhostObject.h>
 #include <BulletDynamics/Character/btKinematicCharacterController.h>
@@ -45,12 +46,108 @@ public:
     void OnDisable();
     void OnUpdate(double time_diff);
 
+    /**
+      * Sets whether the WASD keys are enabled for movement.
+      * @param wasd_enabled Whether the WASD keys are enabled for movement.
+      */
+    void SetWASDEnabled(bool wasd_enabled);
+
+    /**
+      * Gets whether the WASD keys are enabled for movement.
+      * @returns Whether the WASD keys are enabled for movement.
+      */
+    bool GetWASDEnabled() const;
+
+    /**
+      * Sets whether the arrow keys are enabled for movement.
+      * @param arrows_enabled Whether the arrow keys are enabled for movement.
+      */
+    void SetArrowsEnabled(bool arrows_enabled);
+
+    /**
+      * Gets whether the arrow keys are enabled for movement.
+      * @returns Whether the arrow keys are enabled for movement.
+      */
+    bool GetArrowsEnabled() const;
+
+    /**
+      * Sets the speed the player moves at.
+      * @param move_speed The maximum speed, in units per second.
+      */
+    void SetMoveSpeed(float move_speed);
+
+    /**
+      * Gets the speed the player moves at.
+      * @returns The player speed, in units per second.
+      */
+    float GetMoveSpeed() const;
+
+    /**
+      * Sets whether the mouse is enabled for looking around.
+      * @param mouse_enabled Whether the mouse is enabled for looking around.
+      */
+    void SetMouseEnabled(bool mouse_enabled);
+
+    /**
+      * Gets whether the mouse is enabled for looking around.
+      * @returns Whether the mouse is enabled for looking around.
+      */
+    bool GetMouseEnabled() const;
+
+    /**
+      * Sets the mouse sensitivity. Default: 1.0.
+      * @param mouse_sensitivity The mouse sensitivity. Default: 1.0.
+      */
+    void SetMouseSensitivity(float mouse_sensitivity);
+
+    /**
+      * Gets the mouse sensitivity. Default: 1.0.
+      * @returns The mouse sensitivity. Default: 1.0.
+      */
+    float GetMouseSensitivity() const;
+
+    /**
+      * Sets whether the mouse y-axis should be inversed.
+      * @param mouse_y_inversed Whether the mouse y-axis should be inversed.
+      */
+    void SetMouseYInversed(bool mouse_y_inversed);
+
+    /**
+      * Gets whether the mouse y-axis should be inversed.
+      * @returns Whether the mouse y-axis should be inversed.
+      */
+    bool GetMouseYInversed() const;
+
+    /**
+      * Sets whether the character can jump or not.
+      * @param jump_enabled Whether the character can jump or not.
+      */
+    void SetJumpEnabled(bool jump_enabled);
+
+    /**
+      * Gets whether the character can jump or not.
+      * @returns Whether the character can jump or not.
+      */
+    bool GetJumpEnabled() const;
+
 private slots:
     void _HandleKeyPressed(const OIS::KeyEvent& event);
 
+    void _HandleMouseInput(const OIS::MouseEvent& event);
+
+    void _HandleKeyReleased(const OIS::KeyEvent& event);
+
 private:
     btKinematicCharacterController* mBtController;       //!< The bullet's built-in character controller.
-    btPairCachingGhostObject* mBtGhostObject;            //!< The bullet's ghost object.
+    btPairCachingGhostObject* mBtGhostObject;            //!< The bullet's ghost object.;
+    bool mMouseEnabled;         //!< Whether the Mouse is enabled for looking around or not.
+    float mMouseSensitivity;    //!< The sensitivity of the mouse. Default: 1.0.
+    bool mMouseYInversed;       //!< True if the mouse's y-axis should be inversed.
+    btVector3 mMove;            //!< Used to keep track of the movement.
+    float mMoveSpeed;           //!< The speed to move at. Default: 5.0. In units per second.
+    bool mWASDEnabled;          //!< Whether the WASD  keys are enabled for movement or not.
+    bool mArrowsEnabled;        //!< Whether the Arrow keys are enabled for movement or not.
+    bool mJumpEnabled;          //!< Whether the character can jump or not.
 };
 }
 
