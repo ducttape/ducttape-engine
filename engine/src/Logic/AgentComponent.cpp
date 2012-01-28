07936a0303cd33823eab7662d0a03f4d70fb51c0
Vincenzo Comito <clynamen@gmail.com>
2012-01-28 22:05:01 +0100

    fixed include

--- engine/src/Logic/AgentComponent.cpp
+++ engine/src/Logic/AgentComponent.cpp
@@ -6,7 +6,7 @@
 // http://www.gnu.org/licenses/lgpl.html
 // ----------------------------------------------------------------------------
 
-#include "AgentComponent.h"
+#include "AgentComponent.hpp"
 #include <Scene/Node.hpp>
 
 #include <DetourNavMesh.h>
